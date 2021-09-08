86e5c6caa202d3c43842b004a64eba880a582d9c
K-San (Laptop) <konstantin.sandfort@gmail.com>
2021-09-08 20:41:49 +0200

    move in the backend works - no validity check

--- src/main/java/controller/Application.java
+++ src/main/java/controller/Application.java
@@ -15,13 +15,5 @@ public class Application {
     public static void main(String[] args) {
         GameRunner gameRunner = new GameRunner();
         Dice dice = new Dice();
-        System.out.println(dice.rollTheDice());
-        System.out.println(dice.rollTheDice());
-        System.out.println(dice.rollTheDice());
-        System.out.println(dice.rollTheDice());
-        System.out.println(dice.rollTheDice());
-        System.out.println(dice.rollTheDice());
-        System.out.println(dice.rollTheDice());
-
     }
 }
--- src/main/java/controller/GameRunner.java
+++ src/main/java/controller/GameRunner.java
@@ -11,15 +11,35 @@ public class GameRunner {
     // Variables
     private int totalMoves = 0;
     private boolean whiteMove;
+    private Board board;
+    private BoardUpdater boardUpdater;
 
     public GameRunner() {
         init();
     }
 
     public void init() {
-        Board board = new Board();
-        BoardUpdater boardU = new BoardUpdater(board);
-        boardU.fillGameStart();
+        board = new Board();
+        boardUpdater = new BoardUpdater(board);
+        boardUpdater.fillGameStart();
         board.printBoard();
+
+        boardUpdater.movePiece(1, 0, 3, 0);
+        board.printBoard();
+    }
+
+    /**
+     * Tries to move a piece from one position to the other.
+     * Validity is checked in used methods.
+     * @param xFrom     initial x position
+     * @param yFrom     initial y position
+     * @param xTo       target x position
+     * @param yTo       target y position
+     */
+    public void attemptMove(int xFrom, int yFrom, int xTo, int yTo) {
+        // Check validity
+
+        // If check is okay, move piece to requested position
+        boardUpdater.movePiece(xFrom, yFrom, xTo, yTo);
     }
 }
