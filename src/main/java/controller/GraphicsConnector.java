280827f3ec7acf0a0175f926d69bd03a07607743
guil <guillaumebams8@gmail.com>
2021-09-08 21:19:01 +0200

    temporary push for roman

---
+++ src/main/java/controller/GraphicsConnector.java
@@ -0,0 +1,13 @@
+package controller;
+
+import gui.ChessSpot;
+
+import java.util.ArrayList;
+
+public class GraphicsConnector {
+    public GraphicsConnector() {
+    }
+    public static ArrayList<ChessSpot> getMoveAbleSpots(int x, int y){
+        return new ArrayList<>();
+    }
+}
