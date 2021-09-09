589d35f0dcec0c75538c0db3599c16ef2fb0d067
guil <guillaumebams8@gmail.com>
2021-09-09 17:59:25 +0200

    removed the piece objects from the gui,

--- src/main/java/gui/Pieces/Knight.java
+++
@@ -1,7 +0,0 @@
-package gui.Pieces;
-
-public class Knight extends Piece{
-    public Knight(String color, int x, int y) {
-        super(color, "knight", x, y);
-    }
-}
--- src/main/java/gui/Pieces/Rook.java
+++
@@ -1,7 +0,0 @@
-package gui.Pieces;
-
-public class Rook extends Piece{
-    public Rook(String color, int x, int y) {
-        super(color, "rook", x, y);
-    }
-}
