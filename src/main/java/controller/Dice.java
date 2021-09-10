94e1ed81d1b4f8504f79c8325609425643911d69
Dino <dpasic8@gmail.com>
2021-09-10 19:07:10 +0200

    Changed dice class

--- src/main/java/controller/Dice.java
+++ src/main/java/controller/Dice.java
@@ -1,9 +1,19 @@
 package controller;
 
+import java.util.ArrayList;
+
 public class Dice {
 
-    public int rollTheDice() {
-        double rand = (Math.random() * 6) + 1;
-        return (int) rand;
+
+    /**
+     * sees which pieces have a valid play and decides which piece will be rolled based on that
+     * @param usablePieces - a list containing all the characters that represent pieces that have at least one valid move
+     * @return char - the character value representing the piece that will have to be moved in the next play
+     */
+    public char rollTheDice(ArrayList<Character> usablePieces) {
+        int numberOfPieces = usablePieces.size();
+        int random = (int)(Math.random()*numberOfPieces);
+        return  usablePieces.get(random);
+
     }
 }
