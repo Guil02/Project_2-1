28af80c9bfebfac11f8d703d6e45230aea21232f
LAPTOP-IPBN20TN\pmlew <pmlewandowski@gmail.com>
2021-09-23 15:11:45 +0200

    timer is working ;)

---
+++ src/main/java/gui/GameScreenObjects/GoodLabel.java
@@ -0,0 +1,14 @@
+package gui.GameScreenObjects;
+
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+
+public class GoodLabel extends Label {
+
+    public GoodLabel(LeftSide leftSide){
+       setFont(new Font("Verdana", 20));
+       setMinSize(leftSide.getWidthFromChessGUI() * 0.15, leftSide.getHeightFromChessGUI() * 0.7 / 5);
+        setMaxSize(leftSide.getWidthFromChessGUI() * 0.15, leftSide.getHeightFromChessGUI() * 0.7 / 5);
+
+    }
+}
--- src/main/java/gui/GameScreenObjects/LeftSide.java
+++ src/main/java/gui/GameScreenObjects/LeftSide.java
@@ -8,7 +8,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.util.Duration;
 
 
@@ -22,27 +21,17 @@ public class LeftSide extends VBox {
     private int whiteTime = 600;
     private int blackTime = 600;
     private boolean isWhiteTurn = true;
+    private GameScreen gameScreen;
 
     public LeftSide(GameScreen gameScreen) {
-        labelOne = new Label(String.valueOf(blackTime));
-        labelTwo = new Label(String.valueOf(whiteTime));
-        labelOne.setFont(new Font("Verdana", 30));
-        labelTwo.setFont(new Font("Verdana", 30));
-        labelFirst = new Label();
-        labelSecond = new Label();
-        labelThird = new Label();
-        labelFirst.setFont(new Font("Verdana", 20));
-        labelThird.setFont(new Font("Verdana", 20));
-        labelFirst.setMinSize(gameScreen.getWidthFromChessGUI() * 0.15, gameScreen.getHeightFromChessGUI() * 0.7 / 5);
-        labelFirst.setMaxSize(gameScreen.getWidthFromChessGUI() * 0.15, gameScreen.getHeightFromChessGUI() * 0.7 / 5);
-        labelSecond.setMinSize(gameScreen.getWidthFromChessGUI() * 0.15, gameScreen.getHeightFromChessGUI() * 0.7 / 5);
-        labelSecond.setMaxSize(gameScreen.getWidthFromChessGUI() * 0.15, gameScreen.getHeightFromChessGUI() * 0.7 / 5);
-        labelThird.setMinSize(gameScreen.getWidthFromChessGUI() * 0.15, gameScreen.getHeightFromChessGUI() * 0.7 / 5);
-        labelThird.setMaxSize(gameScreen.getWidthFromChessGUI() * 0.15, gameScreen.getHeightFromChessGUI() * 0.7 / 5);
-        labelOne.setMinSize(gameScreen.getWidthFromChessGUI() * 0.15, gameScreen.getHeightFromChessGUI() * 0.7 / 5);
-        labelOne.setMaxSize(gameScreen.getWidthFromChessGUI() * 0.15, gameScreen.getHeightFromChessGUI() * 0.7 / 5);
-        labelTwo.setMinSize(gameScreen.getWidthFromChessGUI() * 0.15, gameScreen.getHeightFromChessGUI() * 0.7 / 5);
-        labelTwo.setMaxSize(gameScreen.getWidthFromChessGUI() * 0.15, gameScreen.getHeightFromChessGUI() * 0.7 / 5);
+        this.gameScreen = gameScreen;
+        labelOne = new GoodLabel(this);
+        labelOne.setText(String.valueOf(blackTime));
+        labelTwo = new GoodLabel(this);
+        labelTwo.setText(String.valueOf(whiteTime));
+        labelFirst = new GoodLabel(this);
+        labelSecond = new GoodLabel(this);
+        labelThird = new GoodLabel(this);
 
         time = new Timeline();
         time.setCycleCount(Timeline.INDEFINITE);
@@ -71,7 +60,16 @@ public class LeftSide extends VBox {
         }
     }
 
+    public double getWidthFromChessGUI() {
+        return gameScreen.getWidthFromChessGUI();
+    }
+
+
     public void changeTimer() {
         isWhiteTurn = !isWhiteTurn;
+    }
+
+    public double getHeightFromChessGUI() {
+        return gameScreen.getHeightFromChessGUI();
     }
 }
