8203e1ed4c28def24b0c018dbaa237f43f74035f
guil <guillaumebams8@gmail.com>
2021-09-23 15:21:06 +0200

    corrected the alignment

--- src/main/java/gui/GameScreenObjects/GoodLabel.java
+++ src/main/java/gui/GameScreenObjects/GoodLabel.java
@@ -6,8 +6,8 @@ import javafx.scene.text.Font;
 public class GoodLabel extends Label {
 
     public GoodLabel(LeftSide leftSide){
-       setFont(new Font("Verdana", 20));
-       setMinSize(leftSide.getWidthFromChessGUI() * 0.15, leftSide.getHeightFromChessGUI() * 0.7 / 5);
+        setFont(new Font("Verdana", 20));
+        setMinSize(leftSide.getWidthFromChessGUI() * 0.15, leftSide.getHeightFromChessGUI() * 0.7 / 5);
         setMaxSize(leftSide.getWidthFromChessGUI() * 0.15, leftSide.getHeightFromChessGUI() * 0.7 / 5);
 
     }
