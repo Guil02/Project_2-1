91bd5bade9bdeacfd8a2d681e2f748ebf41771d6
k-san-official <konstantin.sandfort@gmail.com>
2021-09-30 11:52:24 +0200

    code review on Application and Dice

--- src/main/java/controller/Dice.java
+++ src/main/java/controller/Dice.java
@@ -1,25 +1,37 @@
 package controller;
 
 import model.pieces.ChessPiece;
-
 import java.util.ArrayList;
 
+/**
+ * This class represents a die for the chess game, which determines the kind of piece that needs to be moved.
+ */
 public class Dice {
+
+    /**
+     * Private method to get the current GameRunner instance
+     * @return  current GameRunner
+     */
     private GameRunner gameRunner;
     public Dice(GameRunner gameRunner){
         this.gameRunner = gameRunner;
     }
+
     /**
-     * sees which pieces have a valid play and decides which piece will be rolled based on that
-     * @param usablePieces - a list containing all the characters that represent pieces that have at least one valid move
-     * @return char - the character value representing the piece that will have to be moved in the next play
+     * Sees which pieces have a valid play and decides which piece will be rolled based on that.
+     * @param   usablePieces - a list containing all the characters that represent pieces that have at least one valid move
+     * @return  char - the character value representing the piece that will have to be moved in the next play
      */
     public char choosePiece(ArrayList<Character> usablePieces) {
         int numberOfPieces = usablePieces.size();
-        int random = (int)(Math.random()*numberOfPieces);
-        return usablePieces.get(random);
+        int randomRoll = (int)(Math.random()*numberOfPieces);
+        return usablePieces.get(randomRoll);
     }
 
+    /**
+     * Executes the dice roll and checks if the result has a possible move.
+     * @return  char - the character of the kind of piece that can be moved
+     */
     public char rollTheDice(){
         runValidMoves();
         ArrayList<Character> movablePieces = new ArrayList<>();
@@ -36,12 +48,22 @@ public class Dice {
         return choosePiece(movablePieces);
     }
 
+    /**
+     * Support method to add a piece to the set of movable pieces.
+     * @see #rollTheDice()
+     * @param movablePieces     set of current pieces that can be moved
+     * @param charToAdd         piece to add
+     */
     private void addCharToArray(ArrayList<Character> movablePieces, char charToAdd) {
         if(!movablePieces.contains(charToAdd)){
             movablePieces.add(charToAdd);
         }
     }
 
+    /**
+     * Support method to run all the valid moves.
+     * @see #rollTheDice()
+     */
     public void runValidMoves(){
         ChessPiece[][] pieces = gameRunner.getBoard().getField();
 
@@ -63,6 +85,11 @@ public class Dice {
         }
     }
 
+    /**
+     * Gets the kind of piece that can be moved in the first move.
+     * Only knight and pawn are possible options.
+     * @return  char - kind of piece to move
+     */
     public char firstMoveDiceRoll(){
         double r = Math.random();
         if(r<0.5){
