6d5728bf6099a31d08c7013c7b7bca16835a1b7f
guil <guillaumebams8@gmail.com>
2021-10-02 19:17:24 +0200

    added two files that will improve the way we resize our game

---
+++ src/main/java/gui/utils/DisplaySizeVector.java
@@ -0,0 +1,19 @@
+package gui.utils;
+
+public class DisplaySizeVector {
+    private final double height;
+    private final double width;
+
+    public DisplaySizeVector(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+}
