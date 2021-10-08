f8f48935d7b23959e57b66096918a8cce2aa6a80
k-san-official <konstantin.sandfort@gmail.com>
2021-10-08 11:01:34 +0200

    commented GameRunner.java

--- src/main/java/controller/GameRunner.java
+++ src/main/java/controller/GameRunner.java
@@ -8,7 +8,7 @@ import model.pieces.ChessPiece;
 import java.util.ArrayList;
 
 /**
- * Main part of the backend.
+ * Keeps track of the game loop and acts as the core piece of the controller.
  */
 public class GameRunner {
 
@@ -22,6 +22,9 @@ public class GameRunner {
     private char movablePiece;
     private Dice dice;
 
+    /**
+     * Constructor
+     */
     public GameRunner() {
         chessGUI = new ChessGUI();
         graphicsConnector = new GraphicsConnector(this);
@@ -29,6 +32,9 @@ public class GameRunner {
 
     }
 
+    /**
+     * Initializes the game when it is started.
+     */
     public void init() {
         totalMoves = 0;
         whiteMove = true;
@@ -62,31 +68,58 @@ public class GameRunner {
         totalMoves++;
     }
 
+    /**
+     * Getter for the current game board.
+     * @return  current game board
+     */
     public Board getBoard() {
         return board;
     }
 
+    /**
+     * Getter for the current game board updater.
+     * @return  current game board updater
+     */
     public BoardUpdater getBoardUpdater() {
         return boardUpdater;
     }
 
+    /**
+     * Getter to see, whose turn it is.
+     * @return  white move or not
+     */
     public boolean getWhiteMove(){
         return whiteMove;
     }
 
+    /**
+     * Setter of the current player to move a piece.
+     * @param whiteMove     "true" for white, "false" for black
+     */
     public void setWhiteMove(boolean whiteMove) {
         this.whiteMove = whiteMove;
     }
 
+    /**
+     * Executes the move of a piece.
+     */
     public void doMove(){
         whiteMove = !whiteMove;
         setMovablePiece(dice.rollTheDice());
     }
 
+    /**
+     * Getter of a movable pieces in the game.
+     * @return  char of the piece
+     */
     public char getMovablePiece() {
         return movablePiece;
     }
 
+    /**
+     * Setter of a movable piece in the game.
+     * @param movablePiece  the movable piece to pass
+     */
     public void setMovablePiece(char movablePiece) {
         this.movablePiece = movablePiece;
     }
