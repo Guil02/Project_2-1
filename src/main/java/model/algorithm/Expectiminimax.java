d6e8bef07dbcb394d118d1762bcd47ee9bb73a4f
LAPTOP-IPBN20TN\pmlew <pmlewandowski@gmail.com>
2021-11-13 16:21:36 +0100

    made a tiny bit cleaner

--- src/main/java/model/algorithm/Expectiminimax.java
+++ src/main/java/model/algorithm/Expectiminimax.java
@@ -1,9 +1,36 @@
 package model.algorithm;
 
 public class Expectiminimax {
+    final int UPPER_BOUND=100;
+    final int LOWER_BOUND=-100;
     public Expectiminimax() {
     }
+    public double star1(TreeNode node, int depth){
+       int score = 0;
+       double alpha=0;// we need to get these somehow
+       double beta=0;
+       // i guess bounds = 100 and -100 cus thats our win/loss atm
+        if(depth==0 || !node.hasChildren()){
+            return node.getValue();
+        }
+        int predecessor_scores=0;
+        double succ_prob=1;
+        for(int i=0;i<node.getChildren().size();i++){
+            double probability =node.getChildren().get(i).getProbability();
+            succ_prob -=probability;
+            double current_alpha =(( alpha - UPPER_BOUND*succ_prob-predecessor_scores)/probability);
+            double current_beta = ((beta - LOWER_BOUND*succ_prob-predecessor_scores)/probability);
+            double n_alpha = Math.max(LOWER_BOUND,current_alpha);
+            double n_beta = Math.min(UPPER_BOUND,current_beta);
+            //random bullshit with doing the moves idk ?????
+            // score = expectiminimax i guess? but with alpha and beta dont we need to rewrite the entire minimax to not use Node???
+            /*
+            if(score>=current_beta) return beta;
+            if(score<=current_alpha) return alpha;
 
+             */
+        }
+    }
     public double expectiminimax(TreeNode node, int depth){
         double a;
         node.createChildren();
