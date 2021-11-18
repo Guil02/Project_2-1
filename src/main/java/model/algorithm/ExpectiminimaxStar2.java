f674c5b1a2c4f25140ad26a2223882b0737b2eca
Roman ILIC <romanilic2002@gmail.com>
2021-11-18 12:36:02 +0100

    clear up

--- src/main/java/model/algorithm/ExpectiminimaxStar2.java
+++ src/main/java/model/algorithm/ExpectiminimaxStar2.java
@@ -1,45 +1,54 @@
 package model.algorithm;
 
-import com.google.inject.OutOfScopeException;
-
-import java.util.ArrayList;
-
 public class ExpectiminimaxStar2 extends Expectiminimax {
 
-    public ExpectiminimaxStar2(boolean withPruning) {
+    public ExpectiminimaxStar2(boolean withPruning){
         super(withPruning);
     }
 
-    public void star2(TreeNode node, int depth) {
+    /**
+     * star2 pruning method for the expectimax AI
+     * @param node the current node
+     * @param depth the depth of the corresponding tree
+     */
+    public void star2(TreeNode node, int depth){
 
+        /*
+         * alpha-beta default values
+         */
         double alpha = -100;
         double beta = 100;
 
-        if (depth > 2) {
-
+        /*
+         * if no chance node children layer --> create
+         */
+        if(depth > 2){
             for (TreeNode nodes : node.getChildren()){
                 nodes.createChildren();
             }
-
-        } else {
-            int number_of_branches = node.getChildren().size();
+        }
+        else{
+            int number_of_children = node.getChildren().size();
             int max_number_of_leaves = 0;
 
-            for (int i = 0; i < number_of_branches; i++) {
+            for(int i = 0; i < number_of_children; i++){
                 int number_of_leaves = node.getChildren().get(i).getChildren().size();
-                if (number_of_leaves > max_number_of_leaves) {
+                if (number_of_leaves > max_number_of_leaves){
                     max_number_of_leaves = number_of_leaves;
                 }
             }
+            /*
+             * invistigate chance nodes leaves values and update the bounds (backpropagate the bounds onto the chance node parent)
+             */
             for (int j = 0; j < max_number_of_leaves; j++) {
-                for (int i = 0; i < number_of_branches; i++) {
+                for (int i = 0; i < number_of_children; i++) {
                     double leaf_value = expectiminimax(node.getChildren().get(i).getChildren().get(j), depth-2);
 
                     if (node.getChildren().get(i).getNodeType() == 1) { //max
                         node.getChildren().get(i).updateLowerAndUpperBounds(leaf_value, node.getChildren().get(i).getUpperBound());
                         double new_lowerBound = 0;
 
-                        for (int h = 0; h < number_of_branches; h++) {
+                        for (int h = 0; h < number_of_children; h++) {
                             new_lowerBound += node.getChildren().get(h).getLowerBound() * node.getChildren().get(h).getProbability();
                             node.updateLowerAndUpperBounds(new_lowerBound, node.getUpperBound());
                         }
@@ -48,12 +57,15 @@ public class ExpectiminimaxStar2 extends Expectiminimax {
                         node.getChildren().get(i).updateLowerAndUpperBounds(node.getChildren().get(i).getLowerBound(), leaf_value);
                         double new_upperBound = 0;
 
-                        for (int l = 0; l < number_of_branches; l++) {
+                        for (int l = 0; l < number_of_children; l++) {
                             new_upperBound += node.getChildren().get(i).getUpperBound() * node.getChildren().get(i).getProbability();
                             node.updateLowerAndUpperBounds(node.getLowerBound(), new_upperBound);
                         }
 
                     } else {
+                        /*
+                         * prune the chance nodes leaves when the first child bounds of the same layer are already out of the search window
+                         */
                         if (!(node.isWithinBounds(alpha, beta))) {
                             node.delete();
                             break;
@@ -64,28 +76,36 @@ public class ExpectiminimaxStar2 extends Expectiminimax {
         }
     }
 
-    public double expectiminimax(TreeNode node, int depth) {
+    public double expectiminimax(TreeNode node, int depth){
+
         double a;
-        if (!node.hasChildren()) {
+        if(!node.hasChildren()){
             node.createChildren();
         }
 
-        if (depth == 0 || !node.hasChildren()) {
+        if(depth == 0 || !node.hasChildren()){
             return node.getValue();
-        } else if (node.getNodeType() == 2) {
+
+        }
+        else if(node.getNodeType() == 2){
             a = Double.MAX_VALUE;
-            for (TreeNode children : node.getChildren()) {
+
+            for(TreeNode children : node.getChildren()){
                 a = Math.min(a, expectiminimax(children, depth - 1));
             }
-        } else if (node.getNodeType() == 1) {
+        }
+        else if(node.getNodeType() == 1){
             a = Double.MIN_VALUE;
-            for (TreeNode children : node.getChildren()) {
+
+            for(TreeNode children : node.getChildren()){
                 a = Math.max(a, expectiminimax(children, depth - 1));
             }
-        } else {
+        }
+        else{
             star2(node, depth);
             a = 0;
-            for (TreeNode children : node.getChildren()) {
+
+            for(TreeNode children : node.getChildren()){
                 a = a + (children.getProbability() * expectiminimax(children, depth - 1));
             }
         }
