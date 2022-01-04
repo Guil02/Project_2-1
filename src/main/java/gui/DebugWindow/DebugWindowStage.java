f0685efc8fabaf09d414c2775183a51ff91b3c92
K-San (Laptop) <konstantin.sandfort@gmail.com>
2022-01-04 20:06:14 +0100

    added more debug window components

--- src/main/java/gui/DebugWindow/DebugWindowStage.java
+++ src/main/java/gui/DebugWindow/DebugWindowStage.java
@@ -1,9 +1,12 @@
 package gui.DebugWindow;
 
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -11,24 +14,72 @@ public class DebugWindowStage extends Stage {
     private int plyCount;
     private Text plyCountText;
     BorderPane root;
+    GridPane gridPane;
     private Button playPauseButton;
     private Button stepButton;
+    private HBox bottomRow;
+    private Slider speedSlider; // Slider to set delay in ms for an agent move
+    private boolean isOnPause;
 
     public DebugWindowStage() {
 
         this.setTitle("Debug Info");
         this.setHeight(400);
         this.setWidth(400);
+        isOnPause = false; // Not on pause for agents by default
 
         initComponents();
         this.setScene(new Scene(root));
     }
 
+    /**
+     * Init all the components on the grid.
+     */
     public void initComponents() {
+        // Layouts
         root = new BorderPane();
+        gridPane = new GridPane();
+        bottomRow = new HBox();
+
+        // Ply count
         plyCount = 0;
         plyCountText = new Text("0");
-        root.setCenter(plyCountText);
+
+        // Center grid
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(0, 10, 0, 10));
+        gridPane.add(new Text("Ply count:"), 0, 0);
+        gridPane.add(plyCountText, 1, 0);
+        root.setCenter(gridPane);
+
+        // Bottom row
+        bottomRow.setPadding(new Insets(10, 10, 10, 10));
+        bottomRow.setSpacing(10);
+        bottomRow.setStyle("-fx-background-color: #557799;");
+        playPauseButton = new Button("Pause");
+        playPauseButton.setOnAction(e -> {
+            if (!isOnPause) {
+                playPauseButton.setText("Play");
+                stepButton.setDisable(false);
+                isOnPause = true;
+            }
+            else {
+                playPauseButton.setText("Pause");
+                stepButton.setDisable(true);
+                isOnPause = false;
+            }
+        });
+        stepButton = new Button("Step");
+        stepButton.setOnAction(e -> {
+
+        });
+        stepButton.setDisable(true);
+        speedSlider = new Slider();
+        speedSlider.setMin(0);
+        speedSlider.setMax(50);
+        bottomRow.getChildren().addAll(playPauseButton, stepButton, new Text("Delay:"), speedSlider);
+        root.setBottom(bottomRow);
     }
 
     public void incrementPlyCount() {
