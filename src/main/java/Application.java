0eb53b61acc82112518a26ed0750a54f33d72873
K-San (Laptop) <konstantin.sandfort@gmail.com>
2022-01-13 10:13:01 +0100

    reformatted Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,9 @@ import controller.GameRunner;
 
 /**
  * Application class which contains the executable method to start the game.
+ * @author Guillaume Bams, Roman Ilic, Piotr Lewandowski, Dino Pašić, Konstantin Sandfort, Nawar Zarifeh
+ * @version 3.0
+ * @since 17/01/2022
  */
 public class Application {
     /**
